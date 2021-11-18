cb5a331fdd6836940872c0a2beed6e4e68ea60ef
vikasverma911 <69479320+vikasverma911@users.noreply.github.com>
2021-11-18 19:47:23 +0530

    Update urls.py
    removed bug

--- api/graded_assignments/urls.py
+++ api/graded_assignments/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import GradedAssignmentListView, GradedAssignmentCreateView
+from api.views import GradedAssignmentListView
 
 urlpatterns = [
     path('', GradedAssignmentListView.as_view()),
