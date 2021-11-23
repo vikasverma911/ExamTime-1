888971a3cf0cd4569cb51365e14c743ac747be32
vikasverma911 <69479320+vikasverma911@users.noreply.github.com>
2021-11-23 15:21:50 +0530

    Update routes.js
    added about url

--- src/routes.js
+++ src/routes.js
@@ -7,15 +7,20 @@ import Profile from "./containers/Profile";
 import AssignmentList from "./containers/AssignmentList";
 import AssignmentDetail from "./containers/AssignmentDetail";
 import AssignmentCreate from "./containers/AssignmentCreate";
+import About from "./containers/About";
+import Home from "./containers/Home";
+
 
 const BaseRouter = () => (
   <Hoc>
     <Route path="/login" component={Login} />
     <Route path="/signup" component={Signup} />
     <Route path="/profile/:id" component={Profile} />
-    <Route exact path="/" component={AssignmentList} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/list" component={AssignmentList} />
     <Route exact path="/assignments/:id" component={AssignmentDetail} />
     <Route exact path="/create/" component={AssignmentCreate} />
+    <Route exact path="/about/" component={About} />
   </Hoc>
 );
 
